44b34fb04b682a3158d16f411c697fae0cb7b2b3
Michael Ruhl <michaelruhl14@hotmail.com>
2023-05-03 21:55:40 -0400

    add login screen UI

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,30 +2,30 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 import handlePress from '../helper/onPressHelper';
 import { StyleSheet } from 'react-native';
+import { styled } from 'nativewind';
 
 interface Props {
   title: string;
   onPress: () => void;
+  style?: StyleSheet.NamedStyles<any>;
 }
 
-const Button: React.FC<Props> = ({ title, onPress }) => {
+const StyledTouchableOpacity = styled(TouchableOpacity);
+const StyledText = styled(TouchableOpacity);
+
+const Button: React.FC<Props> = ({ title, onPress, style }) => {
   return (
-    <TouchableOpacity onPress={onPress} style={styles.button}>
-      <Text style={styles.text}>{title}</Text>
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity
+        className="flex justify-center items-center"
+        style={style}
+        onPress={onPress}>
+        <Text className="text-white" onPress={onPress}>
+          {title}
+        </Text>
+      </TouchableOpacity>
+    </>
   );
 };
 
-const styles = StyleSheet.create({
-  button: {
-    display: 'flex',
-    alignItems: 'center',
-    backgroundColor: '#FF8C00',
-    padding: 10,
-    borderRadius: 10,
-  },
-  text: {
-    fontSize: 16,
-  },
-});
 export default Button;
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, ImageBackground } from 'react-native';
+import { styled } from 'nativewind';
+import Button from '../components/Button';
+import LoginField from '../components/LoginFields';
+
+const StyledButton = styled(Button);
+const StyledView = styled(View);
+const StyledLoginField = styled(LoginField);
+const StyledImageBackground = styled(ImageBackground);
+
+const casaLogo = require('./casaLogo.png');
+const rfgLogo = require('./rfgLogo.png');
 
 const LoginScreen = ({ navigation }) => {
   return (
-    <>
-      <Text style={{ fontSize: 48 }}>LoginScreen</Text>
-      <Button title="Log in" onPress={() => navigation.navigate('CaseContactList')} />
-    </>
+    <StyledView className="justify-between items-center gap-3 flex-1 bg-[#345073]">
+      <StyledView className="flex h-1/3 w-screen items-center">
+        <StyledImageBackground className="w-20 h-20 mt-40" source={casaLogo} />
+      </StyledView>
+      <StyledView className="justify-start items-center gap-4 flex h-1/3 w-screen bg-[#345073] pb-20">
+        <StyledLoginField className="flex text-white border-white border border-2 border-rounded rounded-3xl w-[300] h-10" />
+        <StyledButton
+          className="flex bg-[#ea5a4e] text-white rounded-3xl w-40 h-10"
+          title="Sign in"
+          onPress={() => navigation.navigate('CaseContactList')}
+        />
+      </StyledView>
+      <StyledView className="flex h-1/3 w-screen items-center">
+        <StyledImageBackground className="w-60 h-auto pb-20 z-1" source={rfgLogo} />
+      </StyledView>
+    </StyledView>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default LoginScreen;
