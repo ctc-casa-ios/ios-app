58727294bd3e16bcf4e17cb21a5b9d4bb8dce0fc
Michael Ruhl <michaelruhl14@hotmail.com>
2023-05-15 22:04:39 -0400

    cont'd

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:react-native/all', 'plugin:react-native-a11y/all'],
+  extends: ['eslint:recommended', 'plugin:react-native/all', 'plugin:react-native-a11y/required'],
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
