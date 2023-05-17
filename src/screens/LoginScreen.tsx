f22d7d3898b920b30db748e98a144d4742b36a44
Michael Ruhl <michaelruhl14@hotmail.com>
2023-05-17 12:38:37 -0400

    #42 Authentication Flow: Low priority: UI fixes for log in screen. FIX

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -15,8 +15,8 @@ const rfgLogo = require('../assets/rfgLogo.png');
 const LoginScreen = ({ navigation }) => {
   return (
     <StyledView className="justify-between items-center gap-3 flex-1 bg-[#345073]">
-      <StyledView className="flex h-1/3 w-screen items-center">
-        <StyledImage className="w-20 h-20 mt-40" source={casaLogo} />
+      <StyledView className="flex flex-1 flex-col h-1/3 w-screen justify-end">
+        <StyledImage className="w-20 h-20 self-center" source={casaLogo} />
       </StyledView>
       <StyledView className="justify-start items-center gap-4 flex h-1/3 w-screen bg-[#345073] pb-20">
         <StyledLoginField className="flex text-white border-white border border-2 border-rounded rounded-3xl w-[300] h-10" />
