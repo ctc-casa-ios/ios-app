3ed12684f4c4d2eb4ec57ba0ddfdb354f23ea53f
Michael Ruhl <michaelruhl14@hotmail.com>
2023-06-16 09:39:17 -0400

    remove counter from store

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { NativeWindStyleSheet } from 'nativewind';
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createStackNavigator, TransitionPresets } from 'react-navigation-stack';
 
 import { Provider as AuthProvider } from './src/context/AuthContext';
 import { setNavigator } from './src/navigationRef';
@@ -13,22 +13,31 @@ import CaseContactDetailScreen from './src/screens/CaseContactDetailScreen';
 import CaseContactListScreen from './src/screens/CaseContactListScreen';
 import LoginScreen from './src/screens/LoginScreen';
 
+import store from './src/app/store'
+import { Provider } from 'react-redux'
+
 NativeWindStyleSheet.setOutput({
   default: 'native',
 });
 
 // this variable holds the navigation structure of our app
 const switchNavigator = createSwitchNavigator({
-  loginFlow: createStackNavigator({
+  // loginFlow: createStackNavigator({
+  //   Login: {
+  //     screen: LoginScreen,
+  //     navigationOptions: {
+  //       headerShown: false,
+  //     },
+  //   },
+  // }),
+  // mainFlow: createMaterialBottomTabNavigator({
+  caseContactListFlow: createStackNavigator({
     Login: {
       screen: LoginScreen,
       navigationOptions: {
         headerShown: false,
       },
     },
-  }),
-  // mainFlow: createMaterialBottomTabNavigator({
-  caseContactListFlow: createStackNavigator({
     CaseContactList: {
       screen: CaseContactListScreen,
       navigationOptions: {
@@ -41,23 +50,39 @@ const switchNavigator = createSwitchNavigator({
         headerShown: false,
       },
     },
-  }),
-  CaseContactCreateFlow: createStackNavigator({
     CaseContactCreateScreen: {
       screen: CaseContactCreateScreen,
       navigationOptions: {
         headerShown: false,
       },
     },
-  }),
-  AccountFlow: createStackNavigator({
     AccountScreen: {
       screen: AccountScreen,
       navigationOptions: {
         headerShown: false,
       },
     },
-  }),
+    
+  },{
+    defaultNavigationOptions: {
+    ...TransitionPresets.ModalSlideFromBottomIOS,
+  }}),
+  // CaseContactCreateFlow: createStackNavigator({
+  //   CaseContactCreateScreen: {
+  //     screen: CaseContactCreateScreen,
+  //     navigationOptions: {
+  //       headerShown: false,
+  //     },
+  //   },
+  // }),
+  // AccountFlow: createStackNavigator({
+  //   AccountScreen: {
+  //     screen: AccountScreen,
+  //     navigationOptions: {
+  //       headerShown: false,
+  //     },
+  //   },
+  // }),
   // }),
 });
 
@@ -66,11 +91,13 @@ const App = createAppContainer(switchNavigator);
 export default () => {
   return (
     <AuthProvider>
+      <Provider store={store}>
       <App
         ref={(navigator) => {
           setNavigator(navigator);
         }}
       />
+      </Provider>
     </AuthProvider>
   );
 };
